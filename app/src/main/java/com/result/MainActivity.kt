66bca997fd6e24529c58c0792205954bd8c97e64
victor-denisenko <victor.dens@mail.ru>
2020-03-23 15:07:11 +0300

    Initial commit

---
+++ app/src/main/java/com/result/MainActivity.kt
@@ -0,0 +1,31 @@
+package com.result
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.result.ActivityResultCallback
+import androidx.activity.result.ActivityResultRegistry
+import androidx.activity.result.contract.ActivityResultContracts
+import androidx.fragment.app.FragmentActivity
+import kotlinx.android.synthetic.main.activity_main.*
+
+class MainActivity : AppCompatActivity() {
+
+    /**
+     * Issue:
+     * 1) Cannot get result in callback, because [FragmentActivity.onRequestPermissionsResult] don't call super method ([ComponentActivity.onRequestPermissionsResult]).
+     * So [ComponentActivity.mActivityResultRegistry] field cannot get result.
+     *
+     * 2) If VERSION.SDK_INT < 23, then [ActivityResultCallback] callback not called (see [ActivityResultRegistry.invoke] implementation in [ComponentActivity]).
+     */
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        val launcher = prepareCall(ActivityResultContracts.RequestPermission()) { result ->
+            textView.text = "Result is $result"
+        }
+
+        launcher.launch(android.Manifest.permission.CAMERA)
+    }
+}
