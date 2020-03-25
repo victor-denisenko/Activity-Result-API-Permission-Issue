0b28474cbb014a3e94471594c51fc4c5839694b6
victor-denisenko <victor.dens@mail.ru>
2020-03-25 08:56:29 +0300

    add NOTE to comment

--- app/src/main/java/com/result/MainActivity.kt
+++ app/src/main/java/com/result/MainActivity.kt
@@ -12,11 +12,17 @@ import kotlinx.android.synthetic.main.activity_main.*
 class MainActivity : AppCompatActivity() {
 
     /**
+     * Used: androidx.activity:activity:1.2.0-alpha02
+     *
      * Issue:
      * 1) Cannot get result in callback, because [FragmentActivity.onRequestPermissionsResult] don't call super method ([ComponentActivity.onRequestPermissionsResult]).
      * So [ComponentActivity.mActivityResultRegistry] field cannot get result.
      *
      * 2) If VERSION.SDK_INT < 23, then [ActivityResultCallback] callback not called (see [ActivityResultRegistry.invoke] implementation in [ComponentActivity]).
+     *
+     *
+     * NOTE Issue 1 fix: use FragmentActivity from androidx.fragment:fragment:1.3.0-alpha02
+     * NOTE Issue 2 possible fix in next alpha release: ["short-circuit" behavior for requesting permissions](https://issuetracker.google.com/issues/151110799)
      */
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
